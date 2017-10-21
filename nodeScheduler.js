ed1321ed1462ca4a72f0f928d34e738cfa12f9d5
nodes777 <nodes777@gmail.com>
2017-10-21 13:20:50 -0700

    removes env entirely

--- nodeScheduler.js
+++ nodeScheduler.js
@@ -1,14 +1,15 @@
 var exec = require('child_process').exec;
-console.log("Starting scrape");
+console.log("Starting setInterval...");
 setInterval(startScrape, 86400000);
+console.log("Starting scrape...");
 startScrape();
 
 function startScrape(){
-	exec('casperjs scrape.js',
+	exec('casperjs scrape.js', //process.env
 		    {
-		    	env: { '$PATH': './node\\_modules/.bin/',
+		    	/*env: { 'PATH': './node\\_modules/.bin/',
 		    		'deets' : process.env.deets,
-		    	 },
+		    	 },*/
 			},
 		    function(err, stdout, stderr) {
 		        if (err) {
